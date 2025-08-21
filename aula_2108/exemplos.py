c336b32aad83cbd9fd7e94e94e18f34d633c57ae
Pedro Paulo Junqueira <pepejunqueira@hotmail.com>
2025-08-21 19:44:21 -0300

    exemplos de slicing

--- aula_2108/exemplos.py
+++ aula_2108/exemplos.py
@@ -2,3 +2,8 @@ import numpy
 
 #slicing (fatiamento) de conjunto de dados
 
+mtz = numpy.arange(1, 10, 1).reshape(3, 3)
+print(mtz)
+
+print(mtz[2]) #extraindo apenas a terceira linha
+print(mtz[:, 1]) #extraindo apenas a segunda coluna
