921d7aebca1e90cdf6003df17b06b599cf029214
Pedro Paulo Junqueira <pepejunqueira@hotmail.com>
2025-08-21 20:41:45 -0300

    tratamento de string e upload de datasets

--- aula_2108/exemplos.py
+++ aula_2108/exemplos.py
@@ -19,5 +19,15 @@ print(mtz[mtz % 2 == 0]) #retorna só os pares
 
 #tratamento textual
 
-arr = numpy.array(['Goku', 'Goten', 'Gohan', 'Trunks', 'Bulma'])
+arr = numpy.array(['Goku', 'Goten', 'Gohan', 'Trungoks', 'Bulma'])
+print(arr)
 print(numpy.char.find(arr, 'Go')) #retorna um vetor com a posição em que o "Go" aparece em cada string
+print(numpy.char.find(arr, 'Go') >= 0) #retorna true para quem tem "Go"
+print(arr[numpy.char.find(arr, 'Go') >= 0]) #retorna quem tem "Go" no nome
+
+#fiz upload de um dataset
+ds = numpy.loadtxt('space.csv', delimiter=';', dtype=str, encoding='utf-8') #indicar separador (;), converte tudo para string e indica o encoding dos caracteres
+
+#calculando a média do custo do dataset
+ds_cost = ds[1:, 6].astype(float) #parsing para float da coluna dos custos
+print(ds_cost)
